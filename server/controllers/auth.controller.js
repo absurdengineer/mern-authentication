f2c2a0330f3ccad181015bdf55805b2f33f0d7dd
webformulator <mddalam1@gmail.com>
2022-12-24 02:23:54 +0530

    Feat: Implemented Logout API.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -79,7 +79,14 @@ const refresh = async (req, res) => {
     .json({ data: { accessToken }, message: "Refresh Successful" });
 };
 
-const logout = async (req, res) => {
+const logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.status(204).json();
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
   return res.status(200).json({ message: "Logout Successful" });
 };
 
